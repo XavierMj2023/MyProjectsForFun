e805fb8fdf6b400e0c5ef10c204eca91488341fa
ManjDesp <mddes002@gmail.com>
2023-12-11 00:42:43 +0530

    adding reactjs basics - 001

--- MyProjectsForLearn/ReactJS_Practise/ReactJSPrograms/reactjsdemo/src/index.js
+++ MyProjectsForLearn/ReactJS_Practise/ReactJSPrograms/reactjsdemo/src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloWorld from './components/HelloWorld';
+import ReactJSX from './components/ReactJSX';
+import ExpressionReact from './components/ExpressionReact';
+import Attribute from './components/Attribute';
+import MaterialUI from './components/MaterialUI';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //Creating Element
 const element = <h1>Hello, world</h1>;
 
+var ele2 = <h1>Hello MJ</h1>
+
 root.render(
   <React.StrictMode>
-    <App />
+
+   {/*  <App /> */}
+   {/* <HelloWorld/> */}
+   {/* <ReactJSX/> */}
+   {/* <ExpressionReact/> */}
+   {/* <Attribute/> */}
+   <MaterialUI/>
+
   </React.StrictMode>
 );
 
