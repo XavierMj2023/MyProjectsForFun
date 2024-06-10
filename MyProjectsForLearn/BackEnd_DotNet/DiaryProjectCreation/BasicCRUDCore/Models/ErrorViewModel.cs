810e7d9e4eb0cc62647dccac23643797e822a840
ManjDesp <mddes002@gmail.com>
2024-06-10 18:26:13 +0530

    adding Excel docs XLMS and Excel

--- MyProjectsForLearn/BackEnd_DotNet/DiaryProjectCreation/BasicCRUDCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BasicCRUDCore.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
--- MyProjectsForLearn/BackEnd_DotNet/DiaryProjectCreation/DiaryProjectCreation2023/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DiaryProjectCreation2023.MvcApplication" Language="C#" %>
